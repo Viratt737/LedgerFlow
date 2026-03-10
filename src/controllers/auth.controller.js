59264483880272947ff8b5b30d5f365152ca5ed6
virat <viratt737@gmail.com>
2026-03-10 23:46:05 +0530

    Login routes also done

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -26,7 +26,7 @@ async function userRegisterController(req, res) {
 
     res.cookie('token', token);
 
-    res.status(201).json({
+    res.status(200).json({
         user: {
             _id: user._id,
             email: user.email,
@@ -37,8 +37,44 @@ async function userRegisterController(req, res) {
 }
 
 async function userLoginController(req, res) {
-    
+
+    const { email, password } = req.body;
+
+    const user = await userModel
+        .findOne({ email })
+        .select("+password");
+
+    if (!user) {
+        return res.status(401).json({
+            msg: "User doesn't exist or email/password is invalid"
+        });
+    }
+
+    const isValidPassword = await user.comparePassword(password);
+
+    if (!isValidPassword) {
+        return res.status(401).json({
+            msg: "User doesn't exist or email/password is invalid"
+        });
+    }
+
+    const token = jwt.sign(
+        { userId: user._id },
+        process.env.JWT_SECRET,
+        { expiresIn: "3d" }
+    );
+
+    res.cookie("token", token);
+
+    res.status(200).json({
+        user: {
+            _id: user._id,
+            email: user.email,
+            name: user.name
+        },
+        token
+    });
 }
 module.exports = {
-    userRegisterController
+    userRegisterController ,userLoginController
 };
