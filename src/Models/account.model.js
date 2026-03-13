33bcb709ed6110f5be857727baccda5136827aeb
virat <viratt737@gmail.com>
2026-03-13 23:26:39 +0530

    write model how we find data in database using index

--- src/Models/account.model.js
+++ src/Models/account.model.js
@@ -4,7 +4,8 @@ const accountSchema = new mongoose.Schema({
     user:{
         type:mongoose.Schema.Types.ObjectId,
         ref:"user",
-        required: [true, "Account must be associated with user"]
+        required: [true, "Account must be associated with user"],
+        index:true
     },
     status:{
         enum:{
@@ -20,7 +21,7 @@ const accountSchema = new mongoose.Schema({
 },{
     timestamps:true
 })
-
+accountSchema.index({user:1, status :1})
 
 const accountModel = mongoose.model("account", accountSchema)
 module.exports = accountModel
