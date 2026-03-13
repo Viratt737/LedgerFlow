9042108bcc7bd847798c983f774ecd277ca37810
virat <viratt737@gmail.com>
2026-03-13 23:55:19 +0530

    create a routes for account

--- src/Models/account.model.js
+++ src/Models/account.model.js
@@ -8,6 +8,7 @@ const accountSchema = new mongoose.Schema({
         index:true
     },
     status:{
+        type:String,
         enum:{
             values:["ACTIVE", "FROZEN","CLOSE"],
             msg : "Status can be either ACTIVE,FROZEN or CLOSED",
