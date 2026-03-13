379297fd465e05283b7f86b1fb37c35e8efec5e1
virat <viratt737@gmail.com>
2026-03-14 00:05:24 +0530

    correct all mistake and write all logic for creating account

--- src/Models/account.model.js
+++ src/Models/account.model.js
@@ -11,9 +11,9 @@ const accountSchema = new mongoose.Schema({
         type:String,
         enum:{
             values:["ACTIVE", "FROZEN","CLOSE"],
-            msg : "Status can be either ACTIVE,FROZEN or CLOSED",
-            default:"ACTIVE"
-        }
+            msg : "Status can be either ACTIVE,FROZEN or CLOSED"
+        },
+        default:"ACTIVE"
     },
     currency:{
        type:String,
