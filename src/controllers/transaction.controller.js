4d9410dfbf82f1546980e382832c5a39b860456d
virat <viratt737@gmail.com>
2026-03-15 23:29:57 +0530

    create the follow of transaction

--- src/controllers/transaction.controller.js
+++ src/controllers/transaction.controller.js
@@ -2,11 +2,14 @@ const transactionModel = require('../Models/transaction.model');
 const ledgerModel = require('../Models/ledger.model');
 const emailService = require('../services/email.service');
 const accountModel = require('../Models/account.model');
+const mongoose = require('mongoose');
+
 
 async function createTransaction(req, res){
     // check valid request is coming or not
     const{fromAccount , toAccount, amount, idempotencyKey} = req.body;
-     if(!toAccount || !amount || !idempotencyKey || !fromAccount){
+
+    if(!toAccount || !amount || !idempotencyKey || !fromAccount){
         return res.status(400).json({
             msg:"toAccount, amount, fromAccount and idempotencykey are required"
         })
@@ -27,8 +30,81 @@ async function createTransaction(req, res){
     }
 
     // validate idempotency key
-    c
+    const isTransactionAlreadyExists = await transactionModel.findOne({
+        idempotencyKey: idempotencyKey
+    })
+
+    if(isTransactionAlreadyExists){
+        if(isTransactionAlreadyExists.status === "COMPLETED"){
+            return res.status(200).json({
+                msg:"TransactionAlreadyExists"
+            })
+        }
+
+       if(isTransactionAlreadyExists.status === "PENDING"){
+        return res.status(202).json({
+            msg:"Transaction still in processing"
+        })
+       }
+
+        if(isTransactionAlreadyExists.status === "FAILED"){
+        return res.status(500).json({
+            msg:"Transaction processing failed !!"
+        })
+       }
+
+        if(isTransactionAlreadyExists.status === "REVERSED"){
+        return res.status(500).json({
+            msg:"Transaction reversed please retry !!"
+        })
+       }
+    }
+
+    // check account status
+    if(fromUserAccount.status !== "ACTIVE" || toUserAccount.status !== "ACTIVE"){
+        return res.status(404).json({
+            msg : "BOTH fromAccount and toAccount must be ACTIVE to process transaction!!"
+        })
+    }
+    //  check balance is enough to send to other account
+
+    const balance = await fromUserAccount.getBalance();
+
+    if(balance < amount){
+        return res.status(400).json({
+            msg: `Insufficient balance. Current balance is ${balance}. Requested increase your current amount ${amount}`
+        })
+    }
+
+    // 5 create transation(pending)
+
+    const session = await mongoose.startSession()
+    session.startTransaction();
+
+    const transaction = await transactionModel.create({
+        fromAccount,
+        toAccount,
+        amount,
+        idempotencyKey,
+        status: "PENDING"
+    }, {session})
+
+   const debitLedgerEntry = await ledgerModel.create({
+      account : fromAccount,
+      amount : amount,
+      transaction : transaction._id,
+      type : "DEBIT"
+   },{session})
+
+   const creditLedgerEntry = await ledgerModel.create({
+    account : toAccount,
+    amount : amount,
+    transaction : tranaction._id,
+    type: "DEBIT"
+   }, {session})
 }
+
+
 
 async function createInitialFundsTransaction(req, res){
     const {toAccount, amount, idempotencyKey} = req.body;
@@ -60,9 +136,6 @@ async function createInitialFundsTransaction(req, res){
         })
     }
 }
-
-const session = await mongoose.startSession()
-session.startTransaction();
 
 module.exports = {
     createTransaction
